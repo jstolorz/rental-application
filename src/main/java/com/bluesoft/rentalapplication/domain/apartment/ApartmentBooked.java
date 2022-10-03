eee400d4907788c86ce6c6496c2e6900e644e1da
Janusz Stolorz <jstolorz@gmail.com>
2022-10-03 15:12:40 +0200

    Book an Apartment - add getters to ApartmentBooked

--- src/main/java/com/bluesoft/rentalapplication/domain/apartment/ApartmentBooked.java
+++ src/main/java/com/bluesoft/rentalapplication/domain/apartment/ApartmentBooked.java
@@ -11,7 +11,9 @@ class ApartmentBooked {
     private final String apartmentId;
     private final String ownerId;
     private final String tenantId;
-    private final Period period;
+    private final LocalDate periodStart;
+    private final LocalDate periodEnd;
+
 
     private ApartmentBooked(final String eventId, final LocalDateTime eventCreationDateTime, final String apartmentId, final String ownerId, final String tenantId, final Period period) {
 
@@ -20,7 +22,8 @@ class ApartmentBooked {
         this.apartmentId = apartmentId;
         this.ownerId = ownerId;
         this.tenantId = tenantId;
-        this.period = period;
+        periodStart = period.getStart();
+        periodEnd = period.getEnd();
     }
 
     static ApartmentBooked create(String apartmentId, String ownerId, String tenantId, Period period){
@@ -30,4 +33,31 @@ class ApartmentBooked {
         return new ApartmentBooked(eventId,eventCreationDateTime, apartmentId, ownerId,tenantId, period);
     }
 
+    public String getEventId() {
+        return eventId;
+    }
+
+    public LocalDateTime getEventCreationDateTime() {
+        return eventCreationDateTime;
+    }
+
+    public String getApartmentId() {
+        return apartmentId;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public LocalDate getPeriodStart() {
+        return periodStart;
+    }
+
+    public LocalDate getPeriodEnd() {
+        return periodEnd;
+    }
 }
