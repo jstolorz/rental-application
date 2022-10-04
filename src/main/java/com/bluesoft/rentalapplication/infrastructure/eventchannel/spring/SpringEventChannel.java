42d1000dc829290242e279dff5cc14e21f7cfee4
Janusz Stolorz <jstolorz@gmail.com>
2022-10-04 10:47:36 +0200

    Add history about apartment booking - add implementation of EventChannel

---
+++ src/main/java/com/bluesoft/rentalapplication/infrastructure/eventchannel/spring/SpringEventChannel.java
@@ -0,0 +1,25 @@
+package com.bluesoft.rentalapplication.infrastructure.eventchannel.spring;
+
+import com.bluesoft.rentalapplication.domain.apartment.ApartmentBooked;
+import com.bluesoft.rentalapplication.domain.eventchannel.EventChannel;
+import com.bluesoft.rentalapplication.domain.hotelroom.HotelRoomBooked;
+import org.springframework.context.ApplicationEventPublisher;
+
+class SpringEventChannel implements EventChannel {
+
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    SpringEventChannel(final ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+
+    @Override
+    public void publish(final ApartmentBooked apartmentBooked) {
+       applicationEventPublisher.publishEvent(apartmentBooked);
+    }
+
+    @Override
+    public void publish(final HotelRoomBooked hotelRoomBooked) {
+      applicationEventPublisher.publishEvent(hotelRoomBooked);
+    }
+}
