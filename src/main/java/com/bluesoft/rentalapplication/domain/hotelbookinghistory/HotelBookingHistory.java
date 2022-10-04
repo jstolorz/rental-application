cae5d5bc7337660f368d494d02c0d9aa268dabe0
Janusz Stolorz <jstolorz@gmail.com>
2022-10-04 14:29:53 +0200

    Add history about hotel room booking - add implementation of adding history

--- src/main/java/com/bluesoft/rentalapplication/domain/hotelbookinghistory/HotelBookingHistory.java
+++ src/main/java/com/bluesoft/rentalapplication/domain/hotelbookinghistory/HotelBookingHistory.java
@@ -2,21 +2,42 @@ package com.bluesoft.rentalapplication.domain.hotelbookinghistory;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Entity
 public class HotelBookingHistory {
     @Id
     private String hotelId;
 
+    @OneToMany
+    private final List<HotelRoomBookingHistory> hotelRoomBookingHistories = new ArrayList<>();
+
     public HotelBookingHistory(final String hotelId) {
 
         this.hotelId = hotelId;
     }
 
-    public void add(final String hotelRoomId, final LocalDateTime eventCreationDateTime, final String tenantId, final List<LocalDate> days) {
+    public void add(String hotelRoomId, LocalDateTime bookingDateTime, String tenantId, List<LocalDate> days) {
+        HotelRoomBookingHistory hotelRoomBookingHistory = findFor(hotelRoomId);
+        hotelRoomBookingHistory.add(bookingDateTime, tenantId, days);
+    }
 
+    private HotelRoomBookingHistory findFor(String hotelRoomId) {
+        Optional<HotelRoomBookingHistory> history = hotelRoomBookingHistories.stream()
+                .filter(hotelRoomBookingHistory -> hotelRoomBookingHistory.hasIdEqualTo(hotelRoomId))
+                .findFirst();
+
+        if (history.isEmpty()) {
+            HotelRoomBookingHistory hotelRoomBookingHistory = new HotelRoomBookingHistory(hotelRoomId);
+            hotelRoomBookingHistories.add(hotelRoomBookingHistory);
+            return hotelRoomBookingHistory;
+        } else {
+            return history.get();
+        }
     }
 }
